94ebd8c3203d7e2b84f1e37f241e17fbd2281d40
jfz5219 <55001303+jfz5219@users.noreply.github.com>
2019-11-10 14:23:42 -0500

    Update models.py

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Content(models.Model):
@@ -9,3 +10,8 @@ class Content(models.Model):
 
 	def __str__(self):
 		return self.content_title
+
+class User(models.Model):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
